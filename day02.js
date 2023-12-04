a7419b45c5c58915fb0178c772173fdcb535323e
Marcelo Pereira Rodrigues <marcelodive@hotmail.com>
2023-12-04 10:23:24 -0300

    Update day02.js

--- day02.js
+++ day02.js
@@ -2,11 +2,13 @@
 
 // 12 red cubes, 13 green cubes, and 14 blue cubes
 
-const input = `Game 1: 7 green, 14 red, 5 blue; 8 red, 4 green; 6 green, 18 red, 9 blue
-...
-Game 100: 8 green; 2 red, 20 green; 12 green, 1 red, 1 blue; 4 red, 1 blue; 1 blue, 6 red`;
+const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
-// First solution: 2101
+// First solution:
 
 const bag = {
   'red': 12,
@@ -39,7 +41,7 @@ const gameIdSums = Object.keys(gameValidityById).reduce((sum, gameId) => gameVal
 
 console.log(gameIdSums);
 
-// Second solution: 
+// Second solution:
 
 let powersSum = 0;
 
@@ -60,4 +62,3 @@ input.split('\n').forEach((line) => {
 })
 
 console.log(powersSum);
-
